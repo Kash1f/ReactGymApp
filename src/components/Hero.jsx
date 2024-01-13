2b8e289ecc2f7dbd882f6bfa47c18b8222e1d832
Kashif <ahmed.kashif1001@gmail.com>
2024-01-13 23:45:22 +0500

    hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='min-h-screen flex flex-col gap-10 items-center justify-center'>
+        <p>IT'S TIME TO GET</p>
+        <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>Swole<span>normous</span></h1>
+        
+        <p>I hereby acknowledge that I may become <span>unbelievably swolenormous </span> and accept all risks of becoming the local <span>mass montrosity  </span>, afflicted with severe body dismorphia, unable to fit through doors.</p>
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Hero
